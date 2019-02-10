5f3f0167310c440782ae6611ff15fd7f135c70d5
殷啟聰 | Kai-Chung Yan <seamlik@debian.org>
2019-02-10 15:59:24 +0800

    Address: Make all fields mandatory to simplify things

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -9,19 +9,15 @@ pub mod pki;
 /// Combination of an account ID and a device ID.
 ///
 /// It is used to identify an entity a client can interact with. For example, specifying the destination of a message.
-///
-/// Fields are bytes returned from `pki::Certificate::id()` and are optional. However, an
-/// `Address` with the `device` part but not the `account` part is likely invalid in most cases.
 pub struct Address {
-    pub account: Option<Vec<u8>>,
-    pub device: Option<Vec<u8>>,
+    pub account: Vec<u8>,
+    pub device: Vec<u8>,
 }
 
 impl Display for Address {
     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
-        let convert = |x: &Option<Vec<u8>>| match x {
-            Some(data) => data_encoding::HEXUPPER.encode(&data),
-            None => "".to_string(),
+        let convert = |data: &[u8]| {
+            data_encoding::HEXUPPER.encode(&data)
         };
         write!(f, "{}/{}", convert(&self.account), convert(&self.device))
     }
