136389fde0f891ab3d53af66ca908474b90a2f32
殷啟聰 | Kai-Chung Yan <seamlik@debian.org>
2019-08-10 12:10:10 +0800

    Move Riko to a separate repository

--- riko-runtime-java/src/main/java/riko/DoubleFreeException.java
+++
@@ -1,7 +0,0 @@
-package riko;
-
-public class DoubleFreeException extends RuntimeException {
-  public DoubleFreeException() {
-    super("Attempting to free an HeapObject that is already freed!");
-  }
-}
--- riko-runtime-java/src/main/java/riko/UseAfterFreeException.java
+++
@@ -1,7 +0,0 @@
-package riko;
-
-public class UseAfterFreeException extends RuntimeException {
-  public UseAfterFreeException() {
-    super("Attempting to use a HeapObject after it is freed!");
-  }
-}
