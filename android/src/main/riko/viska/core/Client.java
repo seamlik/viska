8643dd7e41622b30b7f291e0855e74ab281c13ee
殷啟聰 | Kai-Chung Yan <seamlik@debian.org>
2019-09-07 17:08:50 +0800

    java: Heap objects extend `riko.Heap`

--- android/src/main/riko/viska/core/Client.java
+++ android/src/main/riko/viska/core/Client.java
@@ -1,19 +1,15 @@
 package viska.core;
 
 import org.checkerframework.checker.nullness.qual.Nullable;
-import riko.DoubleFreeException;
+import riko.Heap;
 import riko.Marshaler;
 import riko.Returned;
-import riko.UseAfterFreeException;
 import riko.UserException;
 
-public class Client implements AutoCloseable {
-
-  private final int handle;
-  private boolean freed;
+public class Client extends Heap {
 
   private Client(final int handle) {
-    this.handle = handle;
+    super(handle);
   }
 
   public static Client create(final String profile_path) throws UserException {
@@ -25,23 +21,14 @@ public class Client implements AutoCloseable {
   private static native byte[] __riko_create(byte[] arg_1);
 
   @Override
-  public void close() {
-    if (freed) {
-      throw new DoubleFreeException();
-    }
-
+  protected void drop() {
     __riko_drop(handle);
-    freed = true;
   }
   private native void __riko_drop(int handle);
 
   public @Nullable String account_id_display() throws UserException {
-    if (freed) {
-      throw new UseAfterFreeException();
-    }
-
+    assertNotFreed();
     final Returned<String> result = Marshaler.fromBytes(__riko_account_id_display(handle));
-
     return result.unwrap();
   }
   private static native byte[] __riko_account_id_display(int handle);
