c78026b2522e39bb96b591ab4202b5205451c631
殷啟聰 | Kai-Chung Yan <seamlik@debian.org>
2020-11-15 18:54:01 +0800

    Android: Don't start the "platform" gRPC daemon any more

--- android/src/main/java/viska/daemon/DaemonService.kt
+++ android/src/main/java/viska/daemon/DaemonService.kt
@@ -1,10 +1,6 @@
 package viska.daemon
 
-import android.util.Log
 import io.grpc.ManagedChannelBuilder
-import io.grpc.Server
-import io.grpc.netty.NettyServerBuilder
-import java.net.InetSocketAddress
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.random.Random
@@ -13,11 +9,8 @@ import org.bson.BsonInt32
 import viska.database.ProfileService
 
 @Singleton
-class DaemonService
-    @Inject
-    constructor(profileService: ProfileService, platformDaemon: PlatformDaemon) : AutoCloseable {
+class DaemonService @Inject constructor(profileService: ProfileService) : AutoCloseable {
 
-  private val platformGrpcServer: Server
   val nodeGrpcClient: NodeGrpcKt.NodeCoroutineStub
   val nodeGrpcServerHandle: Int
 
@@ -29,24 +22,12 @@ class DaemonService
     val localhost = "::1"
 
     // TODO: TLS
-
-    // Platform daemon
-    platformGrpcServer =
-        NettyServerBuilder.forAddress(InetSocketAddress(localhost, 0))
-            .addService(platformDaemon)
-            .build()
-    platformGrpcServer.start()
-    Log.i(
-        javaClass.canonicalName,
-        "Running PlatformDaemon gRPC server at port ${platformGrpcServer.port}")
-
     // Node daemon
     val nodeGrpcPort = Random.nextInt(1, 65536)
     nodeGrpcServerHandle =
         viska.Module.start(
                 BsonBinary(profileService.certificate),
                 BsonBinary(profileService.key),
-                BsonInt32(platformGrpcServer.port),
                 BsonInt32(nodeGrpcPort),
             )
             .asInt32()
@@ -57,6 +38,5 @@ class DaemonService
 
   override fun close() {
     viska.Module.stop(BsonInt32(nodeGrpcServerHandle))
-    platformGrpcServer.shutdown()
   }
 }
