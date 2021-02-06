03713faf3500fdbe9fcc15a1cf97fb245bacfa92
殷啟聰 | Kai-Chung Yan <seamlik@debian.org>
2021-02-07 02:44:21 +0800

    Don't test against `async-executor` as it seems to be buggy

--- core/tests/other-async-executor.rs
+++ core/tests/other-async-executor.rs
@@ -1,4 +1,3 @@
-use async_executor::Executor;
 use futures_executor::ThreadPool;
 use futures_util::task::SpawnExt;
 
@@ -8,18 +7,6 @@ fn run_on_futures_executor() {
     let task = async {
         let (node, task) = viska::util::start_dummy_node().await.unwrap();
         let handle = executor.spawn_with_handle(task).unwrap();
-        drop(node);
-        handle.await;
-    };
-    futures_executor::block_on(task)
-}
-
-#[test]
-fn run_on_async_executor() {
-    let executor = Executor::default();
-    let task = async {
-        let (node, task) = viska::util::start_dummy_node().await.unwrap();
-        let handle = executor.spawn(task);
         drop(node);
         handle.await;
     };
